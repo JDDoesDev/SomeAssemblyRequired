c05733f9147ee7e1e414f68c11fe5536946958cc
JD Flynn <jflynn8@gmail.com>
2023-09-27 22:04:27 -0500

    Initial commit.

---
+++ Body.gd
@@ -0,0 +1,46 @@
+extends CharacterBody3D
+
+var mouse_sensitivity := 0.001
+
+@onready var camera_base = $CameraBase
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const V_LOOK_SENS = 1
+const H_LOOK_SENS = 1
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation.y -= deg_to_rad(event.relative.x * 1)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
---
+++ StateMachine.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var current_state : State
+var states : Dictionary = {}
+
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+			# child.Transitioned.connect(on_child_transition)
